6825aa9be53c2d7abf7f025691b0a27542455de9
[1O16] <youiiyou1253@gmail.com>
2022-10-18 23:05:03 +0900

    typechain

---
+++ build/myPackage.js
@@ -0,0 +1,22 @@
+"use strict";
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+// 타입스크립트 파일에게 자바스크립트 파일을 확인시킴
+// 그저 코멘트이기 때문에 오류가 나지 않음
+// export function init(config) {
+//   return true;
+// }
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns number
+ */
+// export function exit(code) {
+//   return code + 1;
+// }
